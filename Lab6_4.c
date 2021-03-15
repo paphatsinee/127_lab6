17e5550d891bd3da0e37c1c4f32854b82d5a2480
Paphatsinee <paphatsinee.r@ku.th>
2021-03-15 18:52:36 +0700

    Create Lab6_4.c

---
+++ Lab6_4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+        int n;
+        scanf("%d",&n);
+        int a[n][2],g[n];
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<2; j++)
+            {
+                scanf("%d",&a[i][j]);
+            }
+
+        }
+        for(int k=1; k<=n; k++)
+        {
+            for(int i=0; i<n; i++)
+            {
+                for(int j=0; j<2; j++)
+                {
+
+                    if(k==)
+                }
+            }
+        }
+
+}
