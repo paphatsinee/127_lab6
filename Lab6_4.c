fad5f4093761831999a01c59b477aa85cc8168fe
Paphatsinee <paphatsinee.r@ku.th>
2021-03-15 19:04:33 +0700

    Update Lab6_4.c

--- Lab6_4.c
+++ Lab6_4.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-        int n;
+        int n,r;
         scanf("%d",&n);
         int a[n][2],g[n];
         for(int i=0; i<n; i++)
@@ -20,9 +20,21 @@ int main()
                 for(int j=0; j<2; j++)
                 {
 
-                    if(k==)
+                    if(k==a[i][j])
+                    {
+                        g[k]++;
+                    }
                 }
             }
         }
+        for(int k=1; k<=n; k++)
+        {
+            if(g[k]>g[k+1])
+            {
+                r=k;
+            }
+        }
+        printf("%d",r);
 
+        return 0;
 }
